4918c304ebf7ca91084287528a45bbc15d864c10
Alejandro Martinez <alejandro.mzrz@gmail.com>
2022-02-21 15:19:39 +0100

    21 second push

--- routes/memers.js
+++ routes/memers.js
@@ -7,8 +7,6 @@ const User = require("../models/User.model")
 
 router.route("/memers")
     .get((req, res)=>{
-        let session = req.session.username._id
-        console.log(session)
         User.find()
         .then((users)=>{
             res.render("memers-list", {users})
