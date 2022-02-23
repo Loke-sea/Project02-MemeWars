c288d571a1e416c52e6a6bf4e9f9683d55648ed7
Marshall-Bits <kz_@hotmail.es>
2022-02-23 22:51:10 +0100

    cards ready

--- public/scripts/handleDom.js
+++ public/scripts/handleDom.js
@@ -1,14 +1,42 @@
 console.log("JS LOADED");
 
-let displayMemes = document.getElementById("display-memes").style
+function toggleDisplay(itemToDisplay) {
 
-document.getElementById("attack-button").onclick = ()=>{
-    // document.getElementById("display-memes")
-    if(displayMemes.display === 'none'){
-      displayMemes.display = "block"
+  if (itemToDisplay.style.display === 'none') {
+    itemToDisplay.style.display = "flex"
+  }
+  else {
+    itemToDisplay.style.display = "none"
+    console.log(itemToDisplay.style.display);
+  }
+}
+
+window.onload = () => {
+  document.getElementById("search-button").onclick = () => {
+    let searchForm = document.getElementById("search-form")
+    if (searchForm.style.display === 'none') {
+      searchForm.style.display = "flex"
+      document.getElementById("search-button").style.display = "none"
     }
-    else{
-      displayMemes.display = "none"
+    else {
+      searchForm.style.display = "none"
     }
   }
 
+  document.getElementById("menu-button").onclick = () => {
+    let navBarLinks = document.getElementById("navbar-links-container")
+    toggleDisplay(navBarLinks)
+  }
+}
+
+let displayMemes = document.getElementById("display-memes").style
+
+document.getElementById("attack-button").onclick = () => {
+  // document.getElementById("display-memes")
+  if (displayMemes.display === 'none') {
+    displayMemes.display = "block"
+  }
+  else {
+    displayMemes.display = "none"
+  }
+}
